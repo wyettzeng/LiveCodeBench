b9da0813e75dab6f358396438cb148db2497feba
wyett <wyettzeng@gmail.com>
2024-11-13 17:45:40 -0500

    updates

--- myEvaluate/createInference.sh
+++ myEvaluate/createInference.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 model_name=Qwen/Qwen2.5-Coder-7B-Instruct
-python -m lcb_runner.runner.main --model ${model_name} --scenario codegeneration --evaluate
+python -m lcb_runner.runner.main --model ${model_name} --scenario codegeneration --evaluate --trust_remote_code --not_fast
+
+# best of n
+python -m lcb_runner.runner.main --model ${model_name} --scenario codegeneration --evaluate --trust_remote_code --not_fast --n 16 --temperature 1.0
