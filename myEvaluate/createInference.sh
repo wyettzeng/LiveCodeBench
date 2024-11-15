0dc4268bace949d65729175c9e3029e177cca556
wyett <wyettzeng@gmail.com>
2024-11-15 00:58:27 -0500

    updates

--- myEvaluate/createInference.sh
+++ myEvaluate/createInference.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-model_name=Qwen/Qwen2.5-Coder-7B-Instruct
-python -m lcb_runner.runner.main --model "$model_name" --scenario codegeneration --evaluate
+#!/bin/bash
 
-# best of n
-model_name=Qwen/Qwen2.5-Coder-7B-Instruct
-python -m lcb_runner.runner.main --model "$model_name" --scenario codegeneration --evaluate --n 16 --temperature 1.0
+models=(
+  "Qwen/CodeQwen1.5-7B-Chat"
+  # "Qwen/Qwen2.5-Coder-7B-Instruct"
+  "NTQAI/Nxcode-CQ-7B-orpo"
+  "meta-llama/Meta-Llama-3-8B-Instruct"
+)
+
+for model in ${models[@]}
+do
+    # python -m lcb_runner.runner.main --model $model --scenario codegeneration --evaluate
+
+    # best of n
+    python -m lcb_runner.runner.main --model $model --scenario codegeneration --evaluate --n 16 --temperature 1.0
+done
