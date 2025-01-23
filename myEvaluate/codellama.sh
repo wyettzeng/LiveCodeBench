732030d9b1e230f5e83c768510b634e81a61f26f
wyett <wyettzeng@gmail.com>
2025-01-22 20:40:10 -0500

    minor updates

--- myEvaluate/codellama.sh
+++ myEvaluate/codellama.sh
@@ -2,10 +2,10 @@
 
 # specific script used solely for debugging codellama greedy decoding result
 model="codellama/CodeLlama-7b-Instruct-hf"
-# python -m lcb_runner.runner.main --model $model --scenario codegeneration --evaluate --max_tokens 1024 --evaluate --n 32 --temperature 1.0
-# python -m lcb_runner.runner.main --model $model --scenario codegeneration --evaluate --max_tokens 1024 --evaluate --n 64 --temperature 1.0
+# python -m lcb_runner.runner.main --model $model --scenario codegeneration --evaluate --max_tokens 1024 --evaluate --n 32 --temperature 1.0 --release_version "release_v4"
+# python -m lcb_runner.runner.main --model $model --scenario codegeneration --evaluate --max_tokens 1024 --evaluate --n 64 --temperature 1.0 --release_version "release_v4"
 
 
 # for evaluation
-python -m lcb_runner.runner.main --model $model --scenario codegeneration --continue_existing_with_eval --num_process_evaluate 1 --timeout 1 --max_tokens 1024 --evaluate --n 32 --temperature 1.0
-python -m lcb_runner.runner.main --model $model --scenario codegeneration --continue_existing_with_eval --num_process_evaluate 1 --timeout 1 --max_tokens 1024 --evaluate --n 64 --temperature 1.0
+python -m lcb_runner.runner.main --model $model --scenario codegeneration --continue_existing_with_eval --max_tokens 1024 --evaluate --n 32 --temperature 1.0 --release_version "release_v4"
+# python -m lcb_runner.runner.main --model $model --scenario codegeneration --continue_existing_with_eval --max_tokens 1024 --evaluate --n 64 --temperature 1.0 --release_version "release_v4"
---
+++ myEvaluate/mistral.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+model="mistralai/Mistral-7B-Instruct-v0.3"
+
+n_lst=(
+  16
+  32
+  64
+)
+
+python -m lcb_runner.runner.main --model $model --scenario codegeneration --evaluate --release_version release_v4
+
+# for model in ${models[@]}
+# do
+#     python -m lcb_runner.runner.main --model $model --scenario codegeneration --evaluate --release_version release_v4
+
+#     for n in ${n_lst[@]}
+#     do
+#       # best of n
+#       python -m lcb_runner.runner.main --model $model --scenario codegeneration --evaluate --n ${n} --temperature 1.0 --release_version release_v4
+#     done
+# done
