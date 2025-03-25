68508e80ee876fb7cbb0f720c3c4aa73a8c59b6f
wyett <wyettzeng@gmail.com>
2025-03-25 14:19:18 -0400

    updates

--- wyett_helpers/setup.sh
+++ wyett_helpers/setup.sh
@@ -3,8 +3,11 @@
 conda create -n livecodebench python=3.10
 conda init
 conda activate livecodebench
-pip install poetry
-poetry install --with with-gpu
+
+uv venv --python 3.11
+source .venv/bin/activate
+
+uv pip install -e .
 
 # most models are now newer
 pip install transformers --upgrade
